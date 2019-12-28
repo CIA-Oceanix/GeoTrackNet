6e5d3ec72d89dd9d753a1bd8c6ca35ac326b3b3e
dnguyengithub <nvduong0512@gmail.com>
2019-12-28 23:19:28 +0100

    Fixed FLAG f bug.

--- flags_config.py
+++ flags_config.py
@@ -133,6 +133,8 @@ tf.app.flags.DEFINE_boolean("stagger_workers", True,
                             "If true, bring one worker online every 1000 steps.")
 
 # Fix tf >=1.8.0 flags bug
+tf.app.flags.DEFINE_string('f', '', 'kernel')
+
 tf.app.flags.DEFINE_integer("data_dim", 0, "Data dimension")
 tf.app.flags.DEFINE_string('log_filename', '', 'Log filename')
 tf.app.flags.DEFINE_string('logdir_name', '', 'Log dir name')
