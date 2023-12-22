f004ac276f9b18770c0ad7b2f9af17f776f66347
YuchengZhu <1607531044@qq.com>
2023-12-22 14:43:30 +0800

    update csv2pkl2.py

--- data/csv2pkl2.py
+++ data/csv2pkl2.py
@@ -14,25 +14,34 @@ file_cnt=0;
 dp="./CA_data/CA1883"
 data_dict={}
 csv_cnt=0
+# timestamp bnum height speed angle longitude latitude
+bnum=10001
 for fn in os.listdir(dp):
     dataset_path=os.path.join(dp,fn)
     l_l_msg=[]
     ct=0
     with open (dataset_path,"r") as f:
+        
         csvReader=csv.reader(f)
         for row in csvReader:
             ct=ct+1
             if ct==1:
                 continue
+            
             l_l_msg.append([int(row[0]),
-                            (row[2]),float(row[4]),
+                            (bnum),float(row[4]),
                             float(row[5]),float(row[6]),
                             float(row[7]),float(row[8])])
-            print(row)
-    
+            # print(row)
+    print(l_l_msg)
     m_msg=np.array(l_l_msg)
+    print(m_msg)
+    print("==============================")
     data_dict[csv_cnt]=m_msg
+    print(data_dict[csv_cnt])
+    print("--------------------------------")
     csv_cnt=csv_cnt+1
+    bnum=bnum+1
 print(dp)
 dp="./CA1883.pkl"
 print(dp)
--- data/pkl2csv.py
+++ data/pkl2csv.py
@@ -3,8 +3,8 @@ import pickle as pkl
  
 with open ('./CA1883.pkl','rb') as file:
     data=pkl.load(file)
-print(data)
-print(type(data))
+print(data[1][1])
+print(type(data[1][1]))
 # df=pd.DataFrame(data[5])
 # print(df)
 # df.to_csv('output_file2.csv',index=False,header=False)
